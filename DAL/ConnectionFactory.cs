dd7ce0a3161f4a5c417ae705d9f24b8095bd2639
Matheus de Aguiar <102604133+MAguiarP@users.noreply.github.com>
2022-10-20 14:58:15 -0300

    adições npgsql

--- DAL/ConnectionFactory.cs
+++ DAL/ConnectionFactory.cs
@@ -11,5 +11,12 @@ namespace DAL
 {
     public static class ConnectionFactory
     {
+        public static NpgsqlConnection Connect ()
+        {
+            NpgsqlConnection conn = new NpgsqlConnection 
+                 ("Server=localhost; Port=5432;User id=postgres;password=1995;Database=CSHARP;"); 
+            conn.Open ();
+            return conn;
+        }
     }
 }
