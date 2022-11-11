7f05c682e40c54a8173cc4001a0ec040d1c3674e
Matheus Aguiar <matheusdeaguiar@live.com>
2022-11-11 14:14:10 -0300

    AdiçãoClasses

---
+++ DAL/PerfilDAL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using System.Data;
+using Npgsql;
+
+namespace DAL
+{
+    internal class PerfilDAL
+    {
+    }
+}
---
+++ Models/Perfil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Perfil
+    {
+        private string login;
+        private string plano_de_fundo;
+        private string descricao;
+        private string valor;
+        private string cor;
+        private string imagem;
+
+        public string Imagem
+        {
+            get { return imagem; }
+            set { imagem = value; }
+        }
+
+        public string Cor
+        {
+            get { return cor; }
+            set { cor = value; }
+        }
+
+        public string Valor
+        {
+            get { return Valor; }
+            set { Valor = value; }
+        }
+    }
+}
